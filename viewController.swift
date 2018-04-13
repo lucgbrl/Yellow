30c3ea8f1e918dddb799fc4a06bb117fc6558001
lucgbrl <lucassantos@alu.ufc.br>
2018-04-13 17:05:33 -0300

    Add files via upload

--- viewController.swift
+++ viewController.swift
@@ -43,7 +43,78 @@ public class viewController: UIViewController, SFSpeechRecognizerDelegate{
         }
     }//Fim da função
 
-    private func StartRecording() trows { //Tratamento de erros
+   private func StartRecording() throws {
         
+        if let recognitionTask = recognitionTask { //Constante recognitionTast
+            recognitionTask.cancel() //Por algum motivo a captura foi cancelada após ter sido concedido permissão
+            self.recognitionTask = nil //O reconhecimento é encerrado
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        //Os erros abaixo referem-se à situação de o microfone não estar disponível ou não ter sido reconhecido pelo Sistema Operacional em nenhuma tentativa
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else { fatalError("Não houveram entradas no motor de áudio") }
+        guard let recognitionRequest = recognitionRequest else { fatalError("Incapaz de criar sessão SfSpeechAudioBufferRecognitionRequest object")}
+        
+        recognitionRequest.shouldReportPartialResults = true //Irá mostrar até a última parte que foi gravada, antes da falha
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in var isFinal = false
+            
+            if let result = result {
+                self.textview.text = result.bestTranscription.formattedString //caso haja falha durante a gravação será transcrito até o momento anterior à falha de gravação | Continuidade
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal { //Caso o erro não seja a interrupção da gravação mas seja outra coisa
+                self.audioEngine.stop() // A engine de captura irá parar a gravação
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.dictatebutton.isEnabled = true
+                self.dictatebutton.setTitle("Comece a falar", for: [])
+            }
+        
+    }
+    
+        //Outra constante é definida abaixo, definindo o formato de gravação
+    let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        try audioEngine.start()
+        
+        textview.text = "Estou ouvindo!"
+    }
+    
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            dictatebutton.isEnabled = true
+            dictatebutton.setTitle("Iniciar Gravação", for: [])
+        } else {
+            dictatebutton.isEnabled = false
+            dictatebutton.setTitle("Indisponível.", for: .disabled)
+        }
+    }
+    
+    @IBAction func dictateaction() {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            dictatebutton.isEnabled = false
+            dictatebutton.setTitle("Encerrando...", for: .disabled)
+        } else {
+            try! StartRecording()
+            dictatebutton.setTitle("Parando Gravação", for: [])
+        }       
     }
 }
